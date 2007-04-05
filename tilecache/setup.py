af3b344fd60a0300be558636c671cb504585d992
Schuyler Erle <schuyler@nocat.net>
2007-04-05 21:18:18 +0000

    Fixes to make lintian happy.

--- tilecache/setup.py
+++ tilecache/setup.py
@@ -10,6 +10,7 @@ setup(name='TileCache',
       url='http://tilecache.org/',
       packages=['TileCache'],
       scripts=['tilecache.cgi', 'tilecache.fcgi',
+               'tilecache_seed.py',
                'tilecache_clean.py', 'tilecache_http_server.py'],
       data_files=[('/etc', ['tilecache.cfg'])]
      )
