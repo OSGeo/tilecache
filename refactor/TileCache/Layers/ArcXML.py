3d808a892e067fe724c51138919f1ad0a5ee7272
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-07-20 18:47:54 +0000

    Support setting of non-visible layers.

--- refactor/TileCache/Layers/ArcXML.py
+++ refactor/TileCache/Layers/ArcXML.py
@@ -4,16 +4,18 @@ import urllib
 import xml.dom.minidom as m
 
 class ArcXML(MetaLayer):
-    def __init__ (self, name, url = None, **kwargs):
+    def __init__ (self, name, url = None, off_layers = "", **kwargs):
         MetaLayer.__init__(self, name, **kwargs) 
         self.url = url
+        self.off_layers = off_layers
 
     def renderTile(self, tile):
-        layers = []
+        layers = off_layers = []
         for id in self.layers.split(","):
             layers.append('<LAYERDEF id="%s" visible="true" />' % id)
+        for id in self.off_layers.split(","):
+            off_layers.append('<LAYERDEF id="%s" visible="false" />' % id)
         bbox = tile.bounds()
-        #raise Exception(bbox)
         xml = """<?xml version="1.0" encoding="UTF-8" ?>
 <ARCXML version="1.1">
 <REQUEST>
@@ -23,11 +25,12 @@ class ArcXML(MetaLayer):
 <IMAGESIZE height="%s" width="%s" />
 <LAYERLIST >
 %s
+%s
 </LAYERLIST>
 </PROPERTIES>
 </GET_IMAGE>
 </REQUEST>
-</ARCXML>""" % (bbox[0], bbox[1], bbox[2], bbox[3], tile.size()[0], tile.size()[1], "\n".join(layers))
+</ARCXML>""" % (bbox[0], bbox[1], bbox[2], bbox[3], tile.size()[0], tile.size()[1], "\n".join(layers), "\n".join(off_layers))
         xmldata = urllib.urlopen(self.url, xml).read()
         doc = m.parseString(xmldata)
         imgURL = doc.getElementsByTagName("OUTPUT")[0].attributes['url'].value
