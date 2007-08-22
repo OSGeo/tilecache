80fc3c59680734d30614594c1b34697e6e5f515b
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-08-22 15:10:06 +0000

    adds labelcache_map_edge_buffer metadata when rendering tiles, only if the metadata is not already set in the mapfile. This uses a hardcoded additional buffer of 5px -- this is designed to account for the failure in the buffer to include 'shields' in the output. (If you're getting half-cut shields, you'll want to change the hardcoded '5' here to something larger -- if you do so, please email the list and let me know, and I'll make it a configuration option.

--- tilecache/TileCache/Layer.py
+++ tilecache/TileCache/Layer.py
@@ -401,6 +401,16 @@ class MapServerLayer(MetaLayer):
     def renderTile(self, tile):
         import mapscript
         wms = mapscript.mapObj(self.mapfile) 
+        if self.metaBuffer:
+            try:
+                # if the metadata is already set, don't override.
+                wms.getMetaData("labelcache_map_edge_buffer")
+            except mapscript._mapscript.MapServerError:
+                # We stick an extra buffer of 5px in there because in the case
+                # of shields, we want to account for when the shield could get
+                # cut even though the label that the shield is on isn't.
+                buffer = -self.metaBuffer - 5
+                wms.setMetaData("labelcache_map_edge_buffer", str(buffer))
         req = mapscript.OWSRequest()
         req.setParameter("bbox", tile.bbox())
         req.setParameter("width", str(tile.size()[0]))
