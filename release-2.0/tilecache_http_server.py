324ae62c1cebacdcba599a379d22565ed411aed3
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-12-22 16:15:06 +0000

    I am bad at SVN.

--- release-2.0/tilecache_http_server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from TileCache.Service import wsgiApp
-
-if __name__ == '__main__':
-    from wsgiref import simple_server
-    print "Listening on port 8080"
-    httpd = simple_server.WSGIServer(('',8080), simple_server.WSGIRequestHandler,)
-    httpd.set_app(wsgiApp)
-    httpd.serve_forever()
