8e96184ae7debc4c7ab2021cce968e8213287eb5
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-12-22 12:48:29 +0000

    setup.py improvements.

--- tilecache/setup.py
+++ tilecache/setup.py
@@ -2,15 +2,31 @@
 
 from setuptools import setup
 
+readme = file('README','rb').read()
+
+classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        
+]
+
 setup(name='TileCache',
       version='2.0',
       description='a web map tile caching system',
       author='MetaCarta Labs',
       author_email='labs+tilecache@metacarta.com',
       url='http://tilecache.org/',
+      long_description=readme,
       packages=['TileCache', 'TileCache.Caches', 'TileCache.Services', 'TileCache.Layers'],
       scripts=['tilecache.cgi', 'tilecache.fcgi',
                'tilecache_seed.py',
                'tilecache_clean.py', 'tilecache_http_server.py'],
-      data_files=[('/etc', ['tilecache.cfg'])]
+      data_files=[('/etc', ['tilecache.cfg'])],
+      zip_safe=False,
+      license="BSD"
      )
