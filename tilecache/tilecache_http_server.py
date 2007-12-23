7c73f7dd50f13282f29eef7326bfe5dbf49aba47
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-12-23 01:56:54 +0000

    Let the http_server specify a port and a config file on the command line.

--- tilecache/tilecache_http_server.py
+++ tilecache/tilecache_http_server.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 from TileCache.Service import wsgiApp
+from optparse import OptionParser
+
+def run(port=8080, config=None):
+    if config:
+        TileCache.Service.cfgfiles += config  
+    from wsgiref import simple_server
+    httpd = simple_server.WSGIServer(('',port), simple_server.WSGIRequestHandler,)
+    httpd.set_app(wsgiApp)
+    try:
+        httpd.serve_forever()
+        print "Listening on port %s" % port
+    except KeyboardInterrupt:
+        print "Shutting down."
 
 if __name__ == '__main__':
-    from wsgiref import simple_server
-    print "Listening on port 8080"
-    httpd = simple_server.WSGIServer(('',8080), simple_server.WSGIRequestHandler,)
-    httpd.set_app(wsgiApp)
-    httpd.serve_forever()
+    parser = OptionParser()
+    parser.add_option("-p", "--port", help="port to run webserver on. Default is 8080", dest="port", action='store', type="int", default=8080)
+    parser.add_option("-c", "--conf", help="config file to use.", dest="config")
+    (options, args) = parser.parse_args()
+    run(options.port, options.config)
