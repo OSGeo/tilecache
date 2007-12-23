763f2b9159d9e0b6aa00b48158db1bb7092d4aa8
Christopher Schmidt <crschmidt@users.noreply.github.com>
2007-12-23 02:07:46 +0000

    removing the passing of an extra config option -- this doesn't actually work. If this is important, we'll have to find some other way of doing it.

--- tilecache/tilecache_http_server.py
+++ tilecache/tilecache_http_server.py
@@ -3,20 +3,17 @@ from TileCache.Service import wsgiApp
 from optparse import OptionParser
 
 def run(port=8080, config=None):
-    if config:
-        TileCache.Service.cfgfiles += config  
     from wsgiref import simple_server
     httpd = simple_server.WSGIServer(('',port), simple_server.WSGIRequestHandler,)
     httpd.set_app(wsgiApp)
     try:
-        httpd.serve_forever()
         print "Listening on port %s" % port
+        httpd.serve_forever()
     except KeyboardInterrupt:
         print "Shutting down."
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-p", "--port", help="port to run webserver on. Default is 8080", dest="port", action='store', type="int", default=8080)
-    parser.add_option("-c", "--conf", help="config file to use.", dest="config")
     (options, args) = parser.parse_args()
-    run(options.port, options.config)
+    run(options.port)
