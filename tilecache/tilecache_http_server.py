a1fbb175119dfbd542962afefbfdf890bc72027a
Christopher Schmidt <crschmidt@users.noreply.github.com>
2008-03-18 00:15:14 +0000

    Add threading support to standalone http server

--- tilecache/tilecache_http_server.py
+++ tilecache/tilecache_http_server.py
@@ -2,9 +2,15 @@
 from TileCache.Service import wsgiApp
 from optparse import OptionParser
 
-def run(port=8080, config=None):
+def run(port=8080, threading=False, config=None):
     from wsgiref import simple_server
-    httpd = simple_server.WSGIServer(('',port), simple_server.WSGIRequestHandler,)
+    if threading:
+        from SocketServer import ThreadingMixIn
+        class myServer(ThreadingMixIn, simple_server.WSGIServer):
+            pass 
+        httpd = myServer(('',port), simple_server.WSGIRequestHandler,)
+    else:    
+        httpd = simple_server.WSGIServer(('',port), simple_server.WSGIRequestHandler,)
     httpd.set_app(wsgiApp)
     try:
         print "Listening on port %s" % port
@@ -15,5 +21,6 @@ def run(port=8080, config=None):
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-p", "--port", help="port to run webserver on. Default is 8080", dest="port", action='store', type="int", default=8080)
+    parser.add_option("-t", "--threading", help="threading http server. default is false", dest="threading", action='store_true', default=False)
     (options, args) = parser.parse_args()
-    run(options.port)
+    run(options.port, options.threading)
