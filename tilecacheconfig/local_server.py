c641e74a9a62ddc42c9ae5900571dd5a6e29b9e0
Christopher Schmidt <crschmidt@users.noreply.github.com>
2008-05-17 00:42:45 +0000

    * If the TileCache config parsing caused errors, report them.  * Addd a 'save' method which writes the file back out to disk.

--- tilecacheconfig/local_server.py
+++ tilecacheconfig/local_server.py
@@ -2,8 +2,11 @@ from web_request.handlers import wsgi
 
 from tilecacheconfig.Server import run
 
+def run_app(*args, **kwargs):
+    return run("/Users/crschmidt/tilecache/tilecache.cfg", *args, **kwargs) 
+
 def wsgiApp (environ, start_response):
-    return wsgi(run, environ, start_response)
+    return wsgi(run_app, environ, start_response)
 
 
 from wsgiref import simple_server
--- tilecacheconfig/tilecacheconfig/Server.py
+++ tilecacheconfig/tilecacheconfig/Server.py
@@ -1,32 +1,66 @@
 from TileCache.Service import Service
 
+from StringIO import StringIO
+import ConfigParser
+
 from Cheetah.Template import Template
 
-def home(service, parts=None):
+def home(service, parts=None, **kwargs):
     data = Template(open("templates/home_template.tmpl").read(), searchList=[{'layers':service.layers, 'cache':service.cache}])
     return data
 
-def edit(service, parts=None):
-    if not parts or not service.layers.has_key(parts[0]):
+def edit(service, parts=None, **kwargs):
+    if not parts or (not service.layers.has_key(parts[0]) and parts[0] != "cache"):
         return "Error"
     else:
-        layer = service.layers[parts[0]]
-        data = Template(open("templates/edit_layer.tmpl").read(), searchList=[{'layer':layer}])
+        if parts[0] == "cache": 
+            data = Template(open("templates/edit_cache.tmpl").read(), searchList=[{'cache':cache}])
+        else:
+            layer = service.layers[parts[0]]
+            data = Template(open("templates/edit_layer.tmpl").read(), searchList=[{'layer':layer}])
         return str(data)
+
+def save(service, parts=None, params = {}, **kwargs):
+    if not parts or (not service.layers.has_key(parts[0]) and parts[0] != "cache"):
+        return "Error"
+    else:
+        name = params['name']
+        f = open(service.files[0], "w")
+        for key, value in params.items():
+            if key == "name": continue
+            service.config.set(name, key, value)
+        service.config.write(f)
+        f.close()
+        
+        f = open(service.files[0])
+        data = f.read()
+        f.close()
+        
+        return ['text/plain', data]
 
 dispatch_urls = {
  '': home,
  'home': home,
  'edit': edit,
+ 'save': save, 
 } 
 
-def run(path_info = None, **kwargs):
-    s = Service.load("/Users/crschmidt/tilecache/tilecache.cfg")
+def run(config_path = "/etc/tilecache.cfg", path_info = None, **kwargs):
+    s = Service.load(config_path)
+    if s.metadata.has_key('exception'):
+        data = [
+          "Current TileCache config is invalid.", 
+          "Exception: %s" % s.metadata['exception'],
+          "Traceback: \n %s" % s.metadata['traceback']
+        ]
+        return ['text/plain', "\n".join(data)]
     data = ""
     stripped = path_info.strip("/")
     stripped_split = stripped.split("/")
     if dispatch_urls.has_key(stripped_split[0]):
-        data = dispatch_urls[stripped_split[0]](s, parts=stripped_split[1:])
-     
+        data = dispatch_urls[stripped_split[0]](s, parts=stripped_split[1:], **kwargs)
+    
+    if isinstance(data, list):
+        return data
+    
     return ['text/html', str(data)]
-
