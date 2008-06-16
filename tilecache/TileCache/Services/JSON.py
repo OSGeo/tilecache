924539a801e1d28b6e5c18fa117fb5b38aeef4fb
Christopher Schmidt <crschmidt@users.noreply.github.com>
2008-06-16 20:21:26 +0000

    Add support to return layers as an array instead of an object: this is to support the use of this metadata within an Ext JsonReader.

--- tilecache/TileCache/Services/JSON.py
+++ tilecache/TileCache/Services/JSON.py
@@ -4,17 +4,25 @@ import simplejson
 class JSON(TMS):
     def parse(self, fields, path, host):
         layers = {} 
+        type = "object" 
+        if fields.has_key("type") and fields['type'] == "list":
+            layers = []
+            type = "list"    
         for name, layer in self.service.layers.items():
             
-            layers[name] = {
+            data = {
               'bbox': layer.bbox,
               'data_extent': layer.data_extent,
               'resolutions': layer.resolutions,
               'metadata': layer.metadata,
               'srs': layer.srs,
               'units': layer.units,
+              'name': name, 
             }
-        
+            if type == "list":
+                layers.append(data)
+            else:
+                layers[name] = data
         obj = {'layers': layers}
         data = simplejson.dumps(obj)
         if 'callback' in fields:
