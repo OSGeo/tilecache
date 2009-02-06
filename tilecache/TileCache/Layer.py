2181710b2ef76a96a7e3a50f8796a3c70980d897
Christopher Schmidt <crschmidt@users.noreply.github.com>
2009-02-06 00:25:15 +0000

    Don't round the x0 vals, just pull the int part. Patch from Andrew Wooster.

--- tilecache/TileCache/Layer.py
+++ tilecache/TileCache/Layer.py
@@ -276,8 +276,8 @@ class Layer (object):
         x0 = (minx - self.bbox[0]) / (res * self.size[0])
         y0 = (miny - self.bbox[1]) / (res * self.size[1])
         
-        x = int(round(x0))
-        y = int(round(y0))
+        x = int(x0)
+        y = int(y0)
         
         tilex = ((x * res * self.size[0]) + self.bbox[0])
         tiley = ((y * res * self.size[1]) + self.bbox[1])
